571c6d8d9c1cf25d370855d330748636f4c303ee
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2024-04-26 09:11:28 +0530

    first commit

---
+++ 1289. Minimum Falling Path Sum II.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        int ans=INT_MAX;
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        for(int j=0;j<m;j++)dp[0][j]=grid[0][j];
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                int temp=INT_MAX;
+                for(int k=0;k<m;k++){
+                    if(k!=j)temp=min(temp,grid[i][j]+dp[i-1][k]);
+                    dp[i][j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+        ans=min(ans,dp[n-1][i]);
+    }
+    return ans;
+    }
+    
+};
