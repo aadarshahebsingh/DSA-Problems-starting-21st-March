4abf33fadb1ef928df165e194abbe48a9e5ddbc4
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2024-09-14 03:18:02 +0530

    Time: 14 ms (45.89%), Space: 22.8 MB (72.72%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,17 +9,69 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    void inorder(TreeNode* root,vector<int>& arr){
-        if(root==nullptr)return ;
-        inorder(root->left,arr);
-        arr.push_back(root->val);
-        inorder(root->right,arr);
+public: // Using Morris Traversal
+
+    void morrisTraversal(TreeNode* root, int k, int &ans)
+    {
+        TreeNode* curr=root;
+        int cnt=0;
+
+        while(curr!=NULL)
+        {
+            if(curr->left==NULL)
+            {
+                cnt++;
+                if(cnt==k)
+                {
+                    ans=curr->val;
+                }
+                curr=curr->right;
+            }
+            else
+            {
+                TreeNode* pred=curr->left;
+
+                while(pred->right!=NULL && pred->right!=curr)
+                {
+                    pred=pred->right;
+                }
+
+                if(pred->right==NULL)
+                {   
+                    pred->right=curr;
+                    curr=curr->left;
+                }
+                else
+                {
+                    pred->right=NULL;
+
+                    cnt++;
+                    if(cnt==k)
+                    {
+                        ans=curr->val;
+                    }
+                    curr=curr->right;
+                }
+            }
+        }
     }
+
     int kthSmallest(TreeNode* root, int k) {
-       vector<int> arr;
-       inorder(root,arr); 
-       return arr[k-1];
+        
+        int ans=0;
+        morrisTraversal(root, k, ans);
+        return ans;
     }
 };
