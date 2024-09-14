7341ab8ae47a8b56019f49988467ffdd526b23b1
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2024-09-14 16:55:49 +0530

    Time: 16 ms (94.32%), Space: 16.4 MB (85.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,15 @@
-#include <vector>
-#include <algorithm>
-#include <climits> // For INT_MAX
-
-using namespace std;
-
 class Solution {
 public:
-    int helper(vector<int>& coins, int amount, int i, vector<vector<int>>& memo) {
-        if (amount == 0) return 0;
-        if (i == coins.size() || amount < 0) return INT_MAX - 1;
-        if (memo[i][amount] != -1) return memo[i][amount];
-        int takeIt = helper(coins, amount - coins[i], i, memo);
-        if (takeIt != INT_MAX - 1) takeIt++;
-        int notTake = helper(coins, amount, i + 1, memo);
-        return memo[i][amount] = min(takeIt, notTake);
-    }
-    
     int coinChange(vector<int>& coins, int amount) {
-        if (amount == 0) return 0;
-        
-        vector<vector<int>> memo(coins.size(), vector<int>(amount + 1, -1));
-        int result = helper(coins, amount, 0, memo);
-        
-        return result == INT_MAX - 1 ? -1 : result;
+        if(amount==0)return 0;
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(auto x:coins){
+                if(i>=x && dp[i-x]!=INT_MAX)
+                    dp[i]=min(dp[i],dp[i-x]+1);
+            }
+        }
+        return dp[amount]==INT_MAX?-1:dp[amount];
     }
 };
