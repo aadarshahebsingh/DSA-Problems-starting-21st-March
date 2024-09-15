aa0ba259b3987b205c80e2f607aa288ae265dabe
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2024-09-15 06:05:22 +0530

    Time: 31 ms (72.22%), Space: 33.8 MB (90.37%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.cpp
+++ 1428-jump-game-iii/1428-jump-game-iii.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    bool helper(vector<int>& arr, vector<bool>& visited, int i, int n) {
-        if (i < 0 || i >= n || visited[i]) return false;
-        if (arr[i] == 0) return true;
-
-        visited[i] = true;
-
-        bool left = helper(arr, visited, i - arr[i], n);
-        bool right = helper(arr, visited, i + arr[i], n);
-
-        return left || right;
-    }
-
     bool canReach(vector<int>& arr, int start) {
-        vector<bool> visited(arr.size(), false); 
-        return helper(arr, visited, start, arr.size());
+        queue<int> q;
+        q.push(start);
+        int n=arr.size();
+        vector<bool> visited(n,false);
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            visited[curr]=true;
+            int left=curr+arr[curr];
+            int right=curr-arr[curr];
+            if(left>=0 && left<n){
+                if(arr[curr]==0)return true;
+                if(!visited[left])
+                    q.push(left);
+            }
+            if(right>=0 && right<n){
+                if(arr[right]==0)
+                    return true;
+                if(!visited[right])
+                    q.push(right);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]==0 && visited[i])
+                return true;
+        }
+        return false;
     }
 };
