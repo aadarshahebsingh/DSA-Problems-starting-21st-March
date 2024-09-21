3e73ecf3f0bb650dba637612df20b556754411f7
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2024-09-21 19:13:22 +0530

    Time: 500 ms (5.05%), Space: 15.9 MB (36.19%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        // return {};
+        vector<int> ans;
+        for(int i=1;i<=n;i++){
+            ans.push_back(i);
+        }
+        sort(ans.begin(),ans.end(),[](int a,int b){
+            return to_string(a)<to_string(b);
+        });
+        return ans;
+    }
+};
