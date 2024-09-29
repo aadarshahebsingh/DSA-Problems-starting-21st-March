f566b098d404b8b491eea760561cae6e02c22f94
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2024-09-29 20:49:08 +0530

    Time: 86 ms (84.39%), Space: 55 MB (70.06%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.cpp
@@ -1,23 +1,34 @@
 class AllOne {
 public:
+    unordered_map<string,int> count; 
+    set<pair<int,string>> se;      
+    
     AllOne() {
-        
+        count.clear();
     }
-    
+
     void inc(string key) {
-        
+        int n = count[key];  
+        count[key]++;        
+        se.erase({n, key}); 
+        se.insert({n+1, key}); 
     }
-    
+
     void dec(string key) {
-        
+        int n = count[key];  
+        count[key]--;       
+        se.erase({n, key});  
+        if (count[key] > 0) se.insert({n-1, key}); 
+        else count.erase(key); 
     }
-    
+
     string getMaxKey() {
-        
+        if (!se.empty()) return se.rbegin()->second; 
+        return "";
     }
-    
     string getMinKey() {
-        
+        if (!se.empty()) return se.begin()->second;  
+        return "";
     }
 };
 
