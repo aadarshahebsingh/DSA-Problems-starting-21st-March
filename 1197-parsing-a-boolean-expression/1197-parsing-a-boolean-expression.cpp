2c35b06e0232670bcb18f5f216be1ff228213ce8
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2024-10-20 06:56:08 +0530

    Time: 0 ms (100%), Space: 10.5 MB (52.97%) - LeetHub

---
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool parseBoolExpr(string e) {
+        auto it = e.begin();
+        return parse(it);
+    }
+
+    bool parse(string::iterator& it) {
+        switch (*(it++)) {
+            case 't': return true;
+            case 'f': return false;
+            case '|': return parse(it, false, [](bool init, bool val){ return init || val; });
+            case '&': return parse(it, true,  [](bool init, bool val){ return init && val; });
+            case '!': return parse(it, false, [](bool init, bool val){ return !val; });
+        }
+        return false;
+    }
+
+    bool parse(string::iterator& it, bool init, function<bool(bool, bool)> op) {
+        it++; // skip '('
+        while (true) {
+            auto b = parse(it);
+            init = op(init, b);
+            if (*it == ')') break;
+            it++; // skip ','
+        }
+        it++; // skip ')'
+        return init;
+    }
+};
