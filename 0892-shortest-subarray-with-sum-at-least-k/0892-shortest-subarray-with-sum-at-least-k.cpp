254dd78887d0de1ea079a323c881a5dd914a41c2
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2024-11-17 14:04:28 +0530

    Time: 15 ms (98.34%), Space: 97 MB (98.92%) - LeetHub

---
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static int shortestSubarray(vector<int>& nums, int k) {
+        const int n=nums.size();
+        long long sum[n];// prefix sum 0-index
+        sum[0]=nums[0];
+
+        int dp[n], front=0, back=-1, len=1e9;// deque dp
+        for(int r=0; r<n; r++){
+            if (r>0) sum[r]=sum[r-1]+nums[r];
+            if (sum[r]>=k) len=min(len, r+1);
+            while(front<=back && sum[r]-sum[dp[front]]>=k){
+                len=min(len, r-dp[front]);
+                front++;
+            }
+            while(front<=back && sum[r]<=sum[dp[back]]) back--;
+            dp[++back]=r;
+        }
+        return len==1e9?-1:len;
+    }
+};
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
