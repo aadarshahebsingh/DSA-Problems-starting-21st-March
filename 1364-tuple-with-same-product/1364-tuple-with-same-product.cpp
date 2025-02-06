7c9428b5ecd18d40bfcf27cbced1caa24367fc00
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2025-02-06 11:44:29 +0530

    Time: 302 ms (78.06%), Space: 85.1 MB (45.45%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int nC2(int n){
+        return (n*(n-1))/2;
+    }
+    int tupleSameProduct(vector<int>& nums) {
+        int n=nums.size(),cnt=0;
+        unordered_map<int,int> freq;
+
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                freq[nums[i]*nums[j]]++;
+            }
+        }
+
+        for(auto x:freq){
+            cnt+=8*nC2(x.second);
+        }
+        return cnt;
+    }
+};
