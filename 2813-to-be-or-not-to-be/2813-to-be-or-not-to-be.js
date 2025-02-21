b55936fe42b2ffa9908e19b2c494b0e404316770
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2025-02-21 09:21:39 +0530

    Time: 25 ms (100%), Space: 53.6 MB (8.4%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,12 @@
+var expect = function(val) {
+    return {
+        toBe: function(otherVal) {
+            if (val === otherVal) return true;
+            else throw new Error("Not Equal");
+        },
+        notToBe: function(otherVal) {
+            if (val !== otherVal) return true;
+            else throw new Error("Equal");
+        }
+    };
+};
