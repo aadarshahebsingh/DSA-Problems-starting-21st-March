e1cf0d06f0bfb344fea94a58e380112eb6caa6c1
Aadarsh Shaheb Singh <114811139+aadarshahebsingh@users.noreply.github.com>
2026-08-25 07:45:49 +0530

    Time: 300 ms (5.07%), Space: 91.8 MB (62.9%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -1,16 +1,26 @@
+// choose an integer and remove the leftmost x stones from the row
+// add the sum of the removed stones value 
+// new stone whose value is equal to the sum on the leftside of the row
+
+// maximize the score difference
+// bob is to minimize the score difference
 class Solution {
 public:
     int stoneGameVIII(vector<int>& stones) {
-        int n=stones.size();
-        vector<int> prefix(n);
+        vector<int> prefixSum(stones.size());
+        prefixSum[0]=stones[0];
+        for(int i=1;i<stones.size();i++){
+            prefixSum[i]=prefixSum[i-1]+stones[i];
+        }
+       
 
-        prefix[0]=stones[0];
-        for(int i=1;i<n;i++)prefix[i]=prefix[i-1]+stones[i];
-
-        int ans=prefix[n-1];
-
-        for(int i=n-2;i>=1;i--)ans=max(ans,prefix[i]-ans);
-
-        return ans;
+        // for(auto &x:prefixSum)cout<<x<<" ";
+        cout<<endl;
+        int maxi=prefixSum[prefixSum.size()-1];
+        for(int i=stones.size()-2;i>0;i--){
+            maxi=max(maxi,prefixSum[i]-maxi);
+            cout<<maxi<<" ";
+        }
+        return maxi;
     }
 };
